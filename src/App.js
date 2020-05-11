efca2ddb68aae382d0f1f7abca6a7ae3b1262bce
firefly017 <jui.n.nahar@gmail.com>
2020-05-11 12:35:43 +0200

    creating navbar, setting up four static routs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import ListingPage from "./pages/ListingPage";
 import ScheduleViewing from "./pages/ScheduleViewing";
@@ -8,11 +10,16 @@ import AbooutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavBar />
+      </nav>
       <header className="App-header">
-        <Home />
-        <ListingPage />
-        <ScheduleViewing />
-        <AbooutUs />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/listings" component={ListingPage} />
+          <Route path="/scheduleViewing" component={ScheduleViewing} />
+          <Route path="/aboutUs" component={AbooutUs} />
+        </Switch>
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <div>
+      <NavLink
+        activeStyle={{ fontWeight: "bold", color: "black" }}
+        to="/"
+        exact
+      >
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink
+        activeStyle={{ fontWeight: "bold", color: "black" }}
+        to="/listings"
+      >
+        Listings
+      </NavLink>
+      {" | "}
+      <NavLink
+        activeStyle={{ fontWeight: "bold", color: "black" }}
+        to="/scheduleViewing"
+      >
+        Schedule viewing
+      </NavLink>
+      {" | "}
+      <NavLink
+        activeStyle={{ fontWeight: "bold", color: "black" }}
+        to="/aboutUs"
+      >
+        About us
+      </NavLink>
+    </div>
+  );
 }
