56538af84d4c3f0cbb5b33eed12705a7425b2b11
firefly017 <jui.n.nahar@gmail.com>
2020-05-11 12:17:29 +0200

    creating pages, installing react router dom

---
+++ src/pages/ListingPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ListingPage() {
+  return <div>This is the listing page</div>;
+}
---
+++ src/pages/ScheduleViewing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ScheduleViewing() {
+  return <div>this is the schedule viewing page</div>;
+}
